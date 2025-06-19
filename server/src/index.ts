bad2fbcf691bda778323aeb7475a4412c8839c2f
Aniket-jb63 <aniket.jb63@gmail.com>
2025-06-19 15:20:49 +0530

    Fix: add client and server as regular folders

---
+++ server/src/index.ts
@@ -0,0 +1,170 @@
+import express from "express";
+import { random } from "./utils";
+import jwt from "jsonwebtoken";
+import { ContentModel, LinkModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.post("/api/v1/signup", async (req, res) => {
+    // TODO: zod validation , hash the password
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        })
+
+        res.json({
+            message: "User signed up"
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
+})
+
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({ username });
+
+    if (user && user.password === password) {
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_PASSWORD);
+
+        res.json({ token });
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        });
+    }
+})
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+    await ContentModel.create({
+        link,
+        type,
+        title: req.body.title,
+        userId: req.userId,
+        tags: []
+    })
+
+    res.json({
+        message: "Content added"
+    })
+
+})
+
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
+})
+
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId;
+
+    await ContentModel.deleteMany({
+        _id: contentId,
+        userId: req.userId
+    })
+
+    res.json({
+        message: "Deleted"
+    })
+})
+
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share === true;
+    if (share) {
+            const existingLink = await LinkModel.findOne({
+                userId: req.userId
+            });
+
+            if (existingLink) {
+                res.json({
+                    message: "Link already exists",
+                    hash: existingLink.hash
+                })
+                return;
+            }
+            const hash = random(10);
+            await LinkModel.create({
+                userId: req.userId,
+                hash: hash
+            })
+
+            res.json({
+                hash,
+                message: "New link created"
+            })
+    } else {
+        await LinkModel.deleteOne({
+            userId: req.userId
+        });
+
+        res.json({
+            message: "Removed link"
+        })
+    }
+})
+
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink;
+
+    const link = await LinkModel.findOne({
+        hash
+    });
+
+    if (!link) {
+        res.status(411).json({
+            message: "Sorry incorrect input"
+        })
+        return;
+    }
+    // userId
+    const content = await ContentModel.find({
+        userId: link.userId
+    })
+
+    console.log(link);
+    const user = await UserModel.findOne({
+        _id: link.userId
+    })
+
+    if (!user) {
+        res.status(411).json({
+            message: "user not found, error should ideally not happen"
+        })
+        return;
+    }
+
+    res.json({
+        username: user.username,
+        content: content
+    })
+
+})
+
+app.listen(3000, () => {
+    console.log("connected to 3000");
+});
